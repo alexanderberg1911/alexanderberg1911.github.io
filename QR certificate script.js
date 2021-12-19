3730af242fe6e3d660cf0dfe562e92adcf80e238
alexanderberg1911 <alexanderb3rg@protonmail.com>
2021-12-19 16:07:41 +0200

    19-Dec

--- QR certificate script.js
+++ QR certificate script.js
@@ -4,4 +4,4 @@ var myPix = new Array("images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
-document.getElementById("myPicture").src = myPix[randomNum];}
+	 document.getElementById("myPicture").src = myPix[randomNum];}
